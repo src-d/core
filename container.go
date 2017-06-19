03657e5ad1d3968ea04c3141c41091c5cf6d2d79
Antonio Jesus Navarro Perez <antnavper@gmail.com>
2017-06-19 15:32:11 +0200

    update to go-billy v3

--- container.go
+++ container.go
@@ -7,8 +7,8 @@ import (
 
 	"srcd.works/core.v0/model"
 
-	"gopkg.in/src-d/go-billy.v2"
-	"gopkg.in/src-d/go-billy.v2/osfs"
+	"gopkg.in/src-d/go-billy.v3"
+	"gopkg.in/src-d/go-billy.v3/osfs"
 	"srcd.works/framework.v0/configurable"
 	"srcd.works/framework.v0/database"
 	"srcd.works/framework.v0/queue"
